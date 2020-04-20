26592e82d603722bf32eb8d39348da80e896fe36
chetangodiya-simformsolutions <chetan.g@simformsolutions.com>
2020-04-20 14:21:09 +0530

    feat: UNT-T3697 Learn and practice common git concepts 	add subtract,multiplication and division functions in features

---
+++ feature2.js
@@ -0,0 +1,4 @@
+//feature 2 subtraction of two numbers 
+export function sub(num1,nm2){
+    return num1-num2
+}
---
+++ feature3.js
@@ -0,0 +1,8 @@
+//feature 3 multiplication of two numbers
+export function multi(num1,num2){
+    return num1*num2
+}
+//feature 4 divition of two numbers 
+export function div(num1,num2){
+    return num2/num1
+}
