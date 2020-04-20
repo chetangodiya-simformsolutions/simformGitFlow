fff8529cee5ac694b9cd68ae93cf401cbae5c269
chetangodiya-simformsolutions <chetan.g@simformsolutions.com>
2020-04-20 14:21:09 +0530

    feat: UNT-T3697 Learn and practice common git concepts 	add subtract,multiplication and division functions in features with rebase
     #           add subtract,multiplication and division functions in features with rebase

---
+++ feature2.js
@@ -0,0 +1,13 @@
+//feature 2 subtraction of two numbers 
+export function sub(num1,nm2){
+    return num1-num2
+}
+
+//comment for rebase practice
+<<<<<<< HEAD
+
+
+=======
+//another comment for rebase practice
+//another comment for rebase practice
+>>>>>>> 63a9057... second another comment for practice with rebase
