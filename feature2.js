d4e0ecd0fd9b85adb2e091ddfc81526f3f3473cd
chetangodiya-simformsolutions <chetan.g@simformsolutions.com>
2020-04-20 14:21:09 +0530

    feat: UNT-T3697 Learn and practice common git concepts 	add multiplication functions in features

---
+++ feature2.js
@@ -0,0 +1,8 @@
+//feature 2 subtraction of two numbers 
+export function sub(num1,nm2){
+    return num1-num2
+}
+
+//comment for rebase practice
+
+
---
+++ feature3.js
@@ -0,0 +1,6 @@
+//feature 3 multiplication of three numbers
+export function multiwiththree(num1,num2,num3){
+    return num1*num2*num3
+}
+
+//rebase test
